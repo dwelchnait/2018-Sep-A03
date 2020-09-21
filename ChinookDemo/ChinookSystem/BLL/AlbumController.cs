657516901a02860a68d428037d6206ddc208007e
Don Welch <dwelch@nait.ca>
2020-09-21 15:41:36 -0600

    Library album controller setup for CRUD

--- ChinookDemo/ChinookSystem/BLL/AlbumController.cs
+++ ChinookDemo/ChinookSystem/BLL/AlbumController.cs
@@ -8,13 +8,15 @@ using System.Threading.Tasks;
 using ChinookSystem.DAL;
 using ChinookSystem.Entities;
 using ChinookSystem.ViewModels;
-using System.ComponentModel;
+using System.ComponentModel;    //expose for ODS configuration
 #endregion
 
 namespace ChinookSystem.BLL
 {
+    [DataObject]
     public class AlbumController
     {
+        #region Queries
         public List<AlbumArtist> Album_FindByArtist(int artistid)
         {
             using (var context = new ChinookSystemContext())
@@ -32,5 +34,98 @@ namespace ChinookSystem.BLL
                 return results.ToList();
             }
         }
+
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<AlbumList> Album_List()
+        {
+            //return all album records
+            using (var context = new ChinookSystemContext())
+            {
+                var results = from x in context.Albums
+                              select new AlbumList
+                              {
+                                  AlbumId = x.AlbumId,
+                                  Title = x.Title,
+                                  ArtistId = x.ArtistId,
+                                  ReleaseYear = x.ReleaseYear,
+                                  ReleaseLabel = x.ReleaseLabel
+                              };
+                return results.ToList();
+            }
+        }
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public AlbumList Album_FindByID(int albumid)
+        {
+            //return album record matching supply pkey (or null is not found)
+            using (var context = new ChinookSystemContext())
+            {
+                var results = (from x in context.Albums
+                              where x.AlbumId == albumid
+                              select new AlbumList
+                              {
+                                  AlbumId = x.AlbumId,
+                                  Title = x.Title,
+                                  ArtistId = x.ArtistId,
+                                  ReleaseYear = x.ReleaseYear,
+                                  ReleaseLabel = x.ReleaseLabel
+                              }).FirstOrDefault();
+                return results;
+            }
+        }
+        #endregion
+        #region CRUD methods ADD, Update, Delete
+        [DataObjectMethod(DataObjectMethodType.Insert,false)]
+        public void Album_Add(AlbumList item)
+        {
+            using(var context=new ChinookSystemContext())
+            {
+                //moving the data from the external ViewModel instance
+                //  into an internal instance of the entity
+                Album addItem = new Album
+                {
+                    Title = item.Title,
+                    ArtistId = item.ArtistId,
+                    ReleaseYear = item.ReleaseYear,
+                    ReleaseLabel = item.ReleaseLabel
+                };
+                context.Albums.Add(addItem);
+                context.SaveChanges();
+            }
+        }
+        [DataObjectMethod(DataObjectMethodType.Update, false)]
+        public void Album_Update(AlbumList item)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                //moving the data from the external ViewModel instance
+                //  into an internal instance of the entity
+                Album updateItem = new Album
+                {
+                    AlbumId = item.AlbumId,
+                    Title = item.Title,
+                    ArtistId = item.ArtistId,
+                    ReleaseYear = item.ReleaseYear,
+                    ReleaseLabel = item.ReleaseLabel
+                };
+                context.Entry(updateItem).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        [DataObjectMethod(DataObjectMethodType.Delete, false)]
+        public void Album_Delete(AlbumList item)
+        {
+            Album_Delete(item.AlbumId);
+        }
+
+        public void Album_Delete(int albumid)
+        {
+            using(var context = new ChinookSystemContext())
+            {
+                var exists = context.Albums.Find(albumid);
+                context.Albums.Remove(exists);
+                context.SaveChanges();
+            }
+        }
+        #endregion
     }
 }
---
+++ ChinookDemo/ChinookSystem/ViewModels/AlbumList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.ViewModels
+{
+    public class AlbumList
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        public int ReleaseYear { get; set; }
+        public string ReleaseLabel { get; set; }
+    }
+}
